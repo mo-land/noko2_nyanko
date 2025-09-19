b69cde453c002ef53e640757e43beb1d0549e090
mo-land <oh.peach.march@gmail.com>
2025-09-19 17:00:03 +0900

    add: 結果画面へのルーティング・アクションを追加

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,16 @@
 class GamesController < ApplicationController
+  protect_from_forgery with: :null_session  # JSからのPOSTを受けるため
+
   def play
   end
 
   def finish
     # params[:score] からスコアを受け取り保存
     GameRecord.create(score: params[:score])
-    redirect_to ranking_path
+    redirect_to result_path
   end
 
-  def ranking
+  def result
     @records = GameRecord.order(score: :desc).limit(10)
   end
 end
