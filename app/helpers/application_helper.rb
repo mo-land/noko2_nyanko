99a7701360cd663c9b1623eb5c062fd79bb1cc3a
mo-land <oh.peach.march@gmail.com>
2025-09-20 20:45:35 +0900

    add: メタタグを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags(url: "https://res.cloudinary.com/dyafcag5y/image/upload/v1758357387/nnu8puxyejip4rxqgj2w.png")
+    {
+      site: "のこのこにゃんこ",
+      title: "のこのこにゃんこ",
+      reverse: true,
+      charset: "utf-8",
+      description: "「のこのこにゃんこ」は、にゃんこは正義という絶対的ルールのもと、きのこたけのこ狩りを楽しむゲームです。",
+      canonical: request.original_url,
+      separator: "|",
+      og: {
+        site_name: "のこのこにゃんこ",
+        title: "のこのこにゃんこ",
+        description: "「のこのこにゃんこ」は、にゃんこは正義という絶対的ルールのもと、きのこたけのこ狩りを楽しむゲームです。",
+        type: "website",
+        url: request.original_url,
+        locale: "ja_JP"
+      },
+       twitter: {
+        card: "summary_large_image",
+        image: url
+      }
+    }
+  end
 end
